2d33becfd58982ad00e4581a8327465eaf98a9f9
한승완 <hanseung-wan@hanseung-wan-ui-MacBookPro.local>
2022-11-26 17:11:11 +0900

    [Feat] 상품 페이지
    - 상품 리스트
    - 상품 수정 페이지
    - 상품 정보 수정 로직

--- naver/urls.py
+++ naver/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('crawling', views.crawling, name="crawling"),
     path('excel', views.excel, name="excel"),
-    path('shipping/add')
-    path('fileupload', views.fileUpload, name="fileupload")
+    path('fileupload', views.fileUpload, name="fileupload"),
+    path('product', views.ProductListView.as_view(), name="product"),
+    path('product/<int:id>', views.detailView, name="product_detail"),
 ]
