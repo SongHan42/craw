d2767f8adbceabb26c5728b0c0a72ba069953d70
songtofu <songtofu0502@gmail.com>
2022-12-05 19:18:01 +0900

    [Feat] CKEditor
    - Product Model의 상세설명란 wysiwyg 적용

--- naverCrawling/settings.py
+++ naverCrawling/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -31,6 +33,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'ckeditor',
+    'ckeditor_uploader',
     'naver.apps.NaverConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+CKEDITOR_IMAGE_BACKEND = "pillow" 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
